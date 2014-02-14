2230f2a3eacee0cf5e5e2bd339bac4d09ec58960
clement <maboiteaspam@wanadoo.fr>
2014-02-15 01:56:19 +0800

    Release support

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,78 +1,9 @@
 
 module.exports = function(grunt) {
 
-  // test purpose
-  var d = __dirname+"/vendors/phantomizer-html-assets";
-
-  var src_dir = d+"/demo/in/";
-  var out_dir = d+"/demo/out/";
-  var meta_dir = d+"/demo/out/";
-  // test purpose
-
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
-
-    // test purpose
-    ,"out_dir":out_dir
-    ,"meta_dir":meta_dir
-
-    //-
-    ,'phantomizer-html-assets': {
-      options: {
-        "file_suffix": "-opt"
-        ,"in_request": "/some.html"
-        ,"out_path": "<%= out_dir %>/"
-        ,"meta_path": "<%= meta_dir %>/"
-        ,"requirejs_src":"/js/require.js"
-        ,"requirejs_baseUrl": "/js/"
-        ,"manifest": true
-        ,"htmlcompressor": true
-        ,"imgcompressor": true
-        ,"phantomizer-htmlcompressor":{
-          options: {
-            "compress-js":true
-            ,"compress-css":true
-          }
-        }
-        ,"phantomizer-uglifyjs":{
-          banner: ''
-          ,beautify: false
-          ,report: false
-        }
-        ,"phantomizer-requirejs":{
-          "baseUrl": src_dir+"js"
-          ,"paths": {
-            "almond": src_dir+"js/almond-0.2.5"
-            ,"jquery": src_dir+"js/jquery-1.10.2.min"
-          }
-          ,"optimize": "none"
-          ,"wrap": true
-          ,"name": "almond"
-        }
-        ,"phantomizer-imgopt":{
-          "optimizationLevel": 3
-        }
-        ,"img_variance":{
-          "\.(png|jpeg|jpg)$":"@2x.$1"
-        }
-        ,"phantomizer-requirecss":{
-          "optimizeCss": "standard"
-        }
-        ,"paths":[src_dir]
-      }
-      ,test: {
-        options:{
-          "file_suffix": "-test"
-          ,"in_file": src_dir+"/index.html"
-          ,"out": "<%= out_dir %>/index.html"
-          ,"out_path": "<%= out_dir %>/"
-          ,"meta_path": "<%= meta_dir %>/"
-          ,"meta": "<%= meta_dir %>/index-build.html"
-        }
-      }
-    },
-    // test purpose
+    pkg: grunt.file.readJSON('package.json'),
     docco: {
       debug: {
         src: [
@@ -93,12 +24,10 @@ module.exports = function(grunt) {
     },
     release: {
       options: {
-        bump: true,
-        add: false,
-        commit: false,
-        npm: false,
-        npmtag: true,
-        tagName: '<%= version %>',
+        npm: false, //default: true
+        // true will apply the version number as the tag
+        npmtag: true, //default: no tag
+        tagName: '<%= version %>', //default: '<%= version %>'
         github: {
           repo: 'maboiteaspam/phantomizer-html-assets',
           usernameVar: 'GITHUB_USERNAME',
